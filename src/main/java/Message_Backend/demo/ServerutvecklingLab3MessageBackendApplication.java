9c0f4560006217682ba28ab5eac000a4f30a8e72
Ernst Reutergårdh <kedre@kth.se>
2020-11-27 15:37:23 +0100

    -Init Commit

---
+++ src/main/java/Message_Backend/demo/ServerutvecklingLab3MessageBackendApplication.java
@@ -0,0 +1,13 @@
+package Message_Backend.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerutvecklingLab3MessageBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerutvecklingLab3MessageBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Message_Backend/demo/ServerutvecklingLab3MessageBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Message_Backend.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerutvecklingLab3MessageBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
